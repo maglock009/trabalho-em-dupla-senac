8703563915cbe1a14dd309c3fca2c225415578aa
Miguel Dias do Canto <diasdocantomiguel@gmail.com>
2026-06-01 10:27:36 -0300

    acrescentando SQL

---
+++ banco_biblioteca.sql
@@ -0,0 +1,132 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 01/06/2026 às 15:26
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `banco_biblioteca`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `emprestimos`
+--
+
+CREATE TABLE `emprestimos` (
+  `idEmprestimos` int(11) NOT NULL,
+  `TempoEmprestimo` datetime NOT NULL,
+  `ValorEmprestimo` decimal(5,2) NOT NULL,
+  `livros_idlivros` int(11) NOT NULL,
+  `Usuarios_idUsuarios` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `livros`
+--
+
+CREATE TABLE `livros` (
+  `idlivros` int(11) NOT NULL,
+  `Título` varchar(45) NOT NULL,
+  `ISBN` char(13) NOT NULL,
+  `Autor` varchar(150) NOT NULL,
+  `Editora` varchar(150) NOT NULL,
+  `AnoPublicacao` datetime NOT NULL,
+  `QuantidadeLivro` decimal(5,0) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `idUsuarios` int(11) NOT NULL,
+  `Nome` varchar(45) NOT NULL,
+  `CPF` char(11) NOT NULL,
+  `Telefone` varchar(45) NOT NULL,
+  `Email` varchar(100) NOT NULL,
+  `Endereco` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `emprestimos`
+--
+ALTER TABLE `emprestimos`
+  ADD PRIMARY KEY (`idEmprestimos`),
+  ADD KEY `fk_Emprestimos_livros_idx` (`livros_idlivros`),
+  ADD KEY `fk_Emprestimos_Usuarios1_idx` (`Usuarios_idUsuarios`);
+
+--
+-- Índices de tabela `livros`
+--
+ALTER TABLE `livros`
+  ADD PRIMARY KEY (`idlivros`),
+  ADD UNIQUE KEY `ISBN_UNIQUE` (`ISBN`);
+
+--
+-- Índices de tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`idUsuarios`),
+  ADD UNIQUE KEY `CPF_UNIQUE` (`CPF`),
+  ADD UNIQUE KEY `Email_UNIQUE` (`Email`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `emprestimos`
+--
+ALTER TABLE `emprestimos`
+  MODIFY `idEmprestimos` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `livros`
+--
+ALTER TABLE `livros`
+  MODIFY `idlivros` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `idUsuarios` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restrições para tabelas despejadas
+--
+
+--
+-- Restrições para tabelas `emprestimos`
+--
+ALTER TABLE `emprestimos`
+  ADD CONSTRAINT `fk_Emprestimos_Usuarios1` FOREIGN KEY (`Usuarios_idUsuarios`) REFERENCES `usuarios` (`idUsuarios`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_Emprestimos_livros` FOREIGN KEY (`livros_idlivros`) REFERENCES `livros` (`idlivros`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
